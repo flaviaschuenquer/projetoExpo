df4b97e9d6271e1fa63d0214e7827826d1f49851
Flaiva Schuenquer <flavia_schuenquer@hotmail.com>
2021-12-03 00:47:05 -0300

    Adicionando set e setState

--- App.js
+++ App.js
@@ -2,22 +2,50 @@ import React from 'react';
 
 import {Alert, SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
 
-const App = () => {
+/*const App = () => {
   const mostrarAlerta = mensagem => {
     const mensagemPercebida = mensagem;
     Alert.alert(mensagemRecebida);
   };
-
   return (
     <View>
       <SafeAreaView>
-        <Text>Hello world!</Text>
-        <TouchableOpacity onPress={() => mostrarAlerta('Teste')}>
-          <Text>Pressione aqui</Text>
-        </TouchableOpacity>
+        <Text>Meu app</Text>
+        <TouchableOpacity
+          onPress={() => mostrarAlerta('Teste')}></TouchableOpacity>
       </SafeAreaView>
     </View>
   );
 };
+*/
+
+const frases = ['Bom dia!', 'Boa tarde!', 'Boa noite!'];
+class FrasesAleatorias extends Component {
+  constructor() {
+    super();
+    this.state = {
+      frase: 'Bom dia',
+    };
+  }
+
+  trocarFrase = () =>
+    this.setState({frase: frases[Math.floor(Math.random() * frases.length)]});
+  render() {
+    return (
+      <View>
+        <Text style={{fontSize: 50}}>{this.state.frase}</Text>
+        <TouchableOpacity onPress={() => history.trocarFrase()}>
+          <Text style={{fontSize: 30}}>Pressionar</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return <FrasesAleatorias />;
+  }
+}
 
 export default App;
